56423b87703c72280ae8438d1c24643c8f2dea5d
Tgivens2002 <95193648+Tgivens2002@users.noreply.github.com>
2021-11-28 17:15:34 -0700

    done

---
+++ JS/index.js
@@ -0,0 +1,15 @@
+function mouseover2(x){
+    x.style.transition = ".75s";
+}
+function mouseout2(x){
+    x.style.transition = ".75s";
+
+}
+
+window.onload = function Tys(){
+    var welcome = document.getElementById("Tys");
+    welcome.style.opacity = "1";
+    welcome.style.fontSize = "4em";
+    welcome.style.textShadow = "grey 5px 5px 10px";
+    welcome.style.transition = "2s";
+}
